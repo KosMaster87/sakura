4bb3b853d73a5faaa63893be3458b2b64c9b4bcb
Konstantin Aksenov <kav.pbg@gmail.com>
2024-09-08 18:01:13 -0400

    lets go

---
+++ src/app/main-content/find-us/find-us.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-findUs',
+  standalone: true,
+  imports: [],
+  templateUrl: './find-us.component.html',
+  styleUrl: './find-us.component.scss'
+})
+export class FindUsComponent {
+
+}
